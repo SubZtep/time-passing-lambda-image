c7f25e4326734c9bdbfcb93f7ccaf8c66b6a1c4c
Andras Serfozo <subztep@gmail.com>
2021-01-22 23:47:12 +0000

    MVP

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# time-passing-lambda-image
-Netlify Function that calculating the distance from a given date and now in words — rendered on a base64 image
+# Time Passing Lambda Image
+
+Netlify Lambda Function that calculates the distance from a given date and now in words. Display the text on a PNG image and returns in base64 encoded binary format.
+
+## Why?
+
+Intended to mimic GitHub Markdown font style for embedding some dynamic content inside a static text file.
+
+## Usage by example
+
+Embed the Markdown code below to get "_8 month ago_" text in my current moment.
+
+```py
+![07:31 25/05/2020](https://domain/path/ago?time=1590391860000
+```
+
+The `time` parameter is Epoch timestamp in milliseconds.
+
+## Serverless Setup
+
+Deploy the entire repository to Netlify.
---
+++ lambda/ago.js
@@ -0,0 +1,33 @@
+const Jimp = require("jimp")
+const formatDistance = require("date-fns/formatDistance")
+const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+exports.handler = async event => {
+  if (event.queryStringParameters.time === undefined) {
+    return {
+      statusCode: 400,
+      body: "(・・;)ゞ",
+    }
+  }
+
+  const mime = "image/png"
+  const font = await Jimp.loadFont(Jimp.FONT_SANS_14_BLACK)
+  const text = capitalize(formatDistance(new Date(), new Date(+event.queryStringParameters.time).getTime())) + " ago"
+
+  const width = Jimp.measureText(font, text)
+  const height = Jimp.measureTextHeight(font, text)
+  const image = await Jimp.read(width, height + 3)
+
+  image.color([{ apply: "lighten", params: [45] }])
+  image.filterType(Jimp.PNG_FILTER_NONE)
+  image.print(font, 0, 3, text)
+
+  return {
+    isBase64Encoded: true,
+    statusCode: 200,
+    headers: {
+      "Content-Type": mime,
+    },
+    body: (await image.getBase64Async(mime)).split(",", 2).pop(),
+  }
+}
