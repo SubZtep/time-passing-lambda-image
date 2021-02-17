5e3fcfe5bb0ee75a5d6ad5a992bf06dea7e5d10c
Andras Serfozo <subztep@gmail.com>
2021-02-17 13:48:56 +0000

    Load as a file

--- lambda/ago-b.js
+++ lambda/ago-b.js
@@ -1,4 +1,9 @@
+var __create = Object.create;
 var __defProp = Object.defineProperty;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __markAsModule = (target) => __defProp(target, "__esModule", {value: true});
 var __commonJS = (callback, module2) => () => {
   if (!module2) {
@@ -10,6 +15,19 @@ var __commonJS = (callback, module2) => () => {
 var __export = (target, all) => {
   for (var name in all)
     __defProp(target, name, {get: all[name], enumerable: true});
+};
+var __exportStar = (target, module2, desc) => {
+  if (module2 && typeof module2 === "object" || typeof module2 === "function") {
+    for (let key of __getOwnPropNames(module2))
+      if (!__hasOwnProp.call(target, key) && key !== "default")
+        __defProp(target, key, {get: () => module2[key], enumerable: !(desc = __getOwnPropDesc(module2, key)) || desc.enumerable});
+  }
+  return target;
+};
+var __toModule = (module2) => {
+  if (module2 && module2.__esModule)
+    return module2;
+  return __exportStar(__markAsModule(__defProp(module2 != null ? __create(__getProtoOf(module2)) : {}, "default", {value: module2, enumerable: true})), module2);
 };
 
 // node_modules/@babel/runtime/helpers/interopRequireDefault.js
@@ -29688,11 +29706,6 @@ var require_formatDistance = __commonJS((exports2) => {
   }
 });
 
-// node_modules/jimp/fonts/open-sans/open-sans-14-black/open-sans-14-black.fnt
-var require_open_sans_14_black = __commonJS((exports2, module2) => {
-  module2.exports = "open-sans-14-black.PBZWHPUO.fnt";
-});
-
 // lambda/ago.js
 var Jimp = require_dist32();
 
@@ -30283,7 +30296,6 @@ function getTimezoneOffsetInMilliseconds(dirtyDate) {
 
 // lambda/ago.js
 var formatDistance2 = require_formatDistance();
-var fontFile = require_open_sans_14_black();
 var capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 exports.handler = async (event) => {
   if (event.queryStringParameters.time === void 0) {
@@ -30293,7 +30305,7 @@ exports.handler = async (event) => {
     };
   }
   const mime = "image/png";
-  const font = await Jimp.loadFont(require.resolve(fontFile));
+  const font = await Jimp.loadFont(require.resolve("../assets/open-sans-14-black.fnt"));
   const text = capitalize(formatDistance2(new Date(), new Date(+event.queryStringParameters.time).getTime())) + " ago";
   const width = Jimp.measureText(font, text);
   const height = Jimp.measureTextHeight(font, text);
